ca1a598b55cacd646cdb965972325bea8996b97e
Ori <ori100no@gmail.com>
2025-01-28 19:56:00 +0200

    changed description

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,20 +6,19 @@ const Section = styled.section`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  height: 100vh;
 `;
 
 const Home = () => {
   return (
     <Section>
       <H1 align="center">
-        Hi, I'm <span className="text-teal-400">Ori Yossef</span>
-        <br /> a Software developer
+        I'm <span className="text-teal-400">Ori Yossef</span>
+        <br />a Software developer
       </H1>
       <Paragraph align="center" size="lg" weight={400}>
-        I specialize in creating web applications that are reliable, scalable,
-        and secure. I use a variety of programming languages, frameworks, and
-        tools to build custom applications and integrate with existing systems.
+        I enjoy solving complex problems. Frequently praised as detail-oriented
+        by my peers, I can be relied upon to help your company achieve its goals
+        by providing sustainable and scalable solutions.
       </Paragraph>
     </Section>
   );
