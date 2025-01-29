c9976fb165a2752e22218b70078145076a3ca070
Ori <ori100no@gmail.com>
2025-01-29 15:11:09 +0200

    configured aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@components": "/src/ui/components",
+      "@pages": "/src/pages",
+      "@context": "/src/context",
+      "@ui": "/src/ui",
+      "@hooks": "/src/hooks",
+    },
+  },
 });
