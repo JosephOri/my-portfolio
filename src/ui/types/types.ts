1f3695dafe70b522fbd9fd23bc2998da9eee2aa3
Ori <ori100no@gmail.com>
2025-01-29 15:54:25 +0200

    update imports

--- src/ui/types/types.ts
+++ src/ui/types/types.ts
@@ -1,3 +1,20 @@
+import { DefaultTheme } from "styled-components";
+
+export interface Theme extends DefaultTheme {
+  body: string;
+  navbarBg: string;
+  textPrimary: string;
+  textSecondary: string;
+  primaryColor: string;
+  linkColor: string;
+  linkHoverColor: string;
+  cardBg: string;
+  tagBg: string;
+  tagText: string;
+  inputBorder: string;
+  buttonBg: string;
+  buttonHoverBg: string;
+}
 
 export type AlignOptions = "left" | "center" | "right";
 
