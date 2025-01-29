079253ce5a4317d543ba47c4189ad171832286c1
Ori <ori100no@gmail.com>
2025-01-29 16:31:00 +0200

    styled the homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -11,11 +11,11 @@ const Section = styled.section`
 const Home = () => {
   return (
     <Section>
-      <H1 align="center">
+      <H1 align="center" className="mt-5 px-2 md:mt-3">
         I'm <span className="text-teal-400">Ori Yossef</span>
         <br />a Software developer
       </H1>
-      <Paragraph align="center" size="lg" weight={400}>
+      <Paragraph align="center" size="lg" weight={400} className="mt-7 px-3">
         I enjoy solving complex problems. Frequently praised as detail-oriented
         by my peers, I can be relied upon to help your company achieve its goals
         by providing sustainable and scalable solutions.
