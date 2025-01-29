c293324f20fe372c13bbcbe2826f6eb24f15c317
Ori <ori100no@gmail.com>
2025-01-29 22:43:59 +0200

    created about page

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,13 @@
+import { H1 } from "../../ui/components";
+
+const About = () => {
+  return (
+    <div>
+      <H1 size="md" uppercase>
+        let me introduce myself
+      </H1>
+    </div>
+  );
+};
+
+export default About;
