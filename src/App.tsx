0bcb71e473a1a2b467c2c6899a6f84a83ed7e399
Ori <ori100no@gmail.com>
2025-01-30 11:37:43 +0200

    added about page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "styled-components";
 import { darkTheme, lightTheme } from "./ui/theme/theme";
 import Navbar from "./ui/components/Navbar/Navbar";
 import { useThemeMode } from "./context/hooks/useThemeMode";
+import About from "./pages/About/About";
 
 const App = () => {
   const { isDarkMode } = useThemeMode();
@@ -11,6 +12,7 @@ const App = () => {
     <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
       <Navbar />
       <Home />
+      <About />
     </ThemeProvider>
   );
 };
