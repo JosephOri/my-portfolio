82615d1ae1e21540e8e79a7da2daa0c8b879369d
josephori <ori100no@gmail.com>
2025-05-24 15:17:38 +0300

    added ori-image.jpeg to public directory and updated constants file to include new image path

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,6 @@ const programmerImage = "public/programmer2.svg";
 const linkedinUrl = "https://www.linkedin.com/in/ori-joseph-45o/";
 const githubUrl = "https://github.com/josephori";
 const twitterUrl = "https://x.com/josephori30";
+const oriImage = "public/ori-image.jpeg";
 
-export { programmerImage, linkedinUrl, githubUrl, twitterUrl };
+export { linkedinUrl, githubUrl, twitterUrl, oriImage, programmerImage };
